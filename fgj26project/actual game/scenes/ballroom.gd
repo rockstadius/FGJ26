26a782984855a76e205a0c1bc2f1f140864176b5
rockstadius <loanndra@gmail.com>
2026-02-01 14:04:17 +0200

    Updated art & dialogues

--- fgj26project/actual game/scenes/ballroom.gd
+++ fgj26project/actual game/scenes/ballroom.gd
@@ -20,7 +20,7 @@ func _ready() -> void:
 	Dialogic.start("Mission_Brief")
 	
 	
-
+#check every frame
 func _process(delta):
 	if Globals.dialoguefinished == true:
 		harlequin_talk_overlay.visible = false
@@ -28,6 +28,7 @@ func _process(delta):
 		Globals.dialoguefinished = false
 	
 
+#mask selections
 func _on_colombina_button_button_down():
 	Globals.colombina = true
 	gnaga = false
@@ -67,3 +68,23 @@ func _on_dottore_button_pressed():
 	mask_selection_overlay.visible = false
 	Dialogic.start("Day1_Signora_Welcome")
 	return
+
+func _on_signora_pressed():
+	Dialogic.start("Day1_Signora_Welcome")
+	return # Replace with function body.
+
+
+func _on_pierrot_pressed():
+	Dialogic.start("Day1_Pierrot")
+	return # Replace with function body.
+
+
+func _on_pantalone_pressed():
+	Dialogic.start("Day1_Pantalone")
+	return # Replace with function body.
+	
+
+
+func _on_tartaglia_pressed():
+	Dialogic.start("Day1_Tartaglia")
+	return # Replace with function body.
