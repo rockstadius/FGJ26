22238af7b102c1bac756af3ecf7bf08fa26b2679
rockstadius <loanndra@gmail.com>
2026-02-02 22:50:25 +0200

    Updated Web build

--- fgj26project/actual game/Web/index.service.worker.js
+++ fgj26project/actual game/Web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1769966919|36763168738';
+const CACHE_VERSION = '1770064369|57058407756';
 /** @type {string} */
 const CACHE_PREFIX = 'FGJ26Project-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
